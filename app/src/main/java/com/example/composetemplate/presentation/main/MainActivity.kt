233fd47317484948aaf8d670ba0384d65b358e74
Przemysław Niemiec <przemyslaw.niemiec@angrynerds.co>
2021-08-08 13:48:35 +0200

    Added: coroutines, useCase, di modules

--- app/src/main/java/com/example/composetemplate/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/composetemplate/presentation/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.composetemplate
+package com.example.composetemplate.presentation.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -14,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
